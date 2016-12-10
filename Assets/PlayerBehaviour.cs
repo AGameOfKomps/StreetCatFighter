5c615500e0c75aa368c65df8b5e2df19e0c34fd4
musasabi333 <musasabi333@gmail.com>
2016-12-10 17:35:56 +0100

    Added player prefab, changed input manager

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +9,30 @@ public class PlayerBehaviour : MonoBehaviour
     public int Lives = 3;
     public float Speed = 1;
 
+    public PlayerDirection Direction = PlayerDirection.Right;
+
+    public enum PlayerDirection
+    {
+        Left,
+        Right
+    }
+
+    // Keys
+    private float H;
+    private float V;
+    private bool A;
+    private bool B;
+    private bool J;
+
     public enum AttackType
     {
         Weak,
         Heavy
     }
-    
+
+    public GameObject WeakAttackCollider;
+    public GameObject HeavyAttackCollider;
+
     void Start()
     {
 
@@ -21,10 +40,60 @@ public class PlayerBehaviour : MonoBehaviour
     
     void Update()
     {
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
+        H = Input.GetAxis("Horizontal");
+        V = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector2(x, y) * Speed);
+        A = Input.GetButtonDown("Weak Attack");
+        B = Input.GetButtonDown("Heavy Attack");
+
+        J = Input.GetButtonDown("Jump");
+
+        if (H > 0)
+            Direction = PlayerDirection.Right;
+        else if (H < 0)
+            Direction = PlayerDirection.Left;
+
+        if (!A || !B)
+            transform.Translate(new Vector2(H, V) * Speed);
+
+        if (J)
+            Jump();
+
+        if (!J && A)
+            WeakAttack();
+
+        if (!J && B)
+            HeavyAttack();
+    }
+
+    private void WeakAttack()
+    {
+        WeakAttackCollider.SetActive(true);
+
+        // Start animation
+
+        if (Direction == PlayerDirection.Right)
+            WeakAttackCollider.transform.position = new Vector2(-0.5f, 0);
+        else if (Direction == PlayerDirection.Left)
+            WeakAttackCollider.transform.position = new Vector2(0.5f, 0);
+
+        // Do damage
+        WeakAttackCollider.SetActive(false);
+    }
+
+    private void HeavyAttack()
+    {
+        WeakAttackCollider.SetActive(true);
+
+        // Start animation
+
+        if (Direction == PlayerDirection.Right)
+            WeakAttackCollider.transform.position = new Vector2(-0.5f, 0);
+        else if (Direction == PlayerDirection.Left)
+            WeakAttackCollider.transform.position = new Vector2(0.5f, 0);
+
+        // Do damage
+        WeakAttackCollider.SetActive(false);
     }
 
     public void ReceiveHit(float damage)
@@ -33,6 +102,11 @@ public class PlayerBehaviour : MonoBehaviour
 
         if (Energy <= 0)
             Die();
+    }
+
+    void Jump()
+    {
+
     }
 
     void Die()
