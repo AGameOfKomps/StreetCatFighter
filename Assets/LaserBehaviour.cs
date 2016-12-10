c5a2c358e82959fab2cd7b42d5fda62792439639
FimChe <a.serafimovski@yahoo.co.uk>
2016-12-11 00:46:27 +0100

    Refactoring

--- Assets/LaserBehaviour.cs
+++ Assets/LaserBehaviour.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class LaserBehaviour : MonoBehaviour {
 
-    public GameObject Player;
     public int PlayerOffset = 2;
+
+    private GameObject player;
+
 	// Use this for initialization
 	void Start () {
-        Player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 	
 	// Update is called once per frame
 	public void Point () {
         float r = Random.Range(0, 1);
         if (r < 0.25f)
-            transform.position = new Vector2( Player.transform.position.x - PlayerOffset, Player.transform.position.y - PlayerOffset);
-        else if(r>=0.25f && r<0.5f)
-            transform.position = new Vector2(Player.transform.position.x - PlayerOffset, Player.transform.position.y + PlayerOffset);
-        else if (r>=0.5f && r<0.75f)
-            transform.position = new Vector2(Player.transform.position.x + PlayerOffset, Player.transform.position.y - PlayerOffset);
-        else if(r>=0.75f)
-            transform.position = new Vector2(Player.transform.position.x + PlayerOffset, Player.transform.position.y + PlayerOffset);
+            transform.position = new Vector2( player.transform.position.x - PlayerOffset, player.transform.position.y - PlayerOffset);
+        else if(r >= 0.25f && r < 0.5f)
+            transform.position = new Vector2(player.transform.position.x - PlayerOffset, player.transform.position.y + PlayerOffset);
+        else if (r >= 0.5f && r < 0.75f)
+            transform.position = new Vector2(player.transform.position.x + PlayerOffset, player.transform.position.y - PlayerOffset);
+        else if(r >= 0.75f)
+            transform.position = new Vector2(player.transform.position.x + PlayerOffset, player.transform.position.y + PlayerOffset);
 
-        Player.GetComponent<PlayerBehaviour>().LaserOn = true;
+        player.GetComponent<PlayerBehaviour>().LaserOn = true;
     }
 
 
