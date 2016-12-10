549838af77cb9d4a95c30bead66e4009adc9a5e3
musasabi333 <musasabi333@gmail.com>
2016-12-10 14:15:41 +0100

    Moved work from broken repo

---
+++ Assets/BasicEnemyBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyBehavior : MonoBehaviour {
+
+    public int Speed;
+    public int Health;
+    public GameObject Player;
+    
+    // Use this for initialization
+	void Start () {
+        Speed = 1;
+        Health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsOnTarget())
+        {
+            ReceiveHit(0);
+            if (Health > 0)
+            {
+                DeliverHit();
+            }
+            else
+            {
+                Die();
+            }
+        }
+        else
+        {
+            Move();
+        }
+	}
+
+    bool IsOnTarget()
+    {
+        return false;
+    }
+
+    void Move()
+    {
+        Vector2 distance = Player.transform.position - transform.position;
+        Vector2 delta = distance.normalized * Speed;
+        transform.Translate(delta.sqrMagnitude > distance.sqrMagnitude ? distance : delta);
+    }
+
+    void DeliverHit()
+    {
+
+    }
+
+    public void ReceiveHit(int HitPoints)
+    {
+        Health -= HitPoints;
+    }
+
+    void Die()
+    {
+
+    }
+}
---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float Energy;
+    public int Lives;
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+
+    }
+}
