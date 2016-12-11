7d4cde51e3064884bf79981bf1e8f167788c81ee
FimChe <a.serafimovski@yahoo.co.uk>
2016-12-11 06:55:04 +0100

    Camera follows Player

---
+++ Assets/CameraBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+    private GameObject player;
+    private bool chasePlayer;
+    
+    // Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        chasePlayer = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (chasePlayer && transform.position != player.transform.position)
+        {
+            Vector2 distance = player.transform.position - transform.position;
+            Vector2 delta = distance.normalized * player.GetComponent<PlayerBehaviour>().Speed;
+            transform.Translate(delta.sqrMagnitude > distance.sqrMagnitude ? distance : delta);
+        }
+        else
+        {
+            chasePlayer = false;
+        }
+	}
+
+    public void triggerChase()
+    {
+        chasePlayer = true;
+    }
+}
---
+++ Assets/CameraColliderBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraColliderBehavior : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag.Equals("Player"))
+        {
+            GetComponentInParent<CameraBehavior>().triggerChase();
+        }
+    }
+}
